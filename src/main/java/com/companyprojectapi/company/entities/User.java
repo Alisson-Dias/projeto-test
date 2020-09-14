97e6231985db2de0f61674141990940f37d0bfb7
Alisson Silva <alisson.s.dias@hotmail.com>
2020-09-14 14:58:10 -0300

    orderStatus

--- src/main/java/com/companyprojectapi/company/entities/User.java
+++ src/main/java/com/companyprojectapi/company/entities/User.java
@@ -1,13 +1,20 @@
 package com.companyprojectapi.company.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -20,6 +27,10 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User(){}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -84,5 +95,8 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+	public List<Order> getOrders() {
+		return orders;
 	}
 }
