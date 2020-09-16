3646e41aafd4df3135f676d5786d7a58c85cf314
Alisson Silva <alisson.s.dias@hotmail.com>
2020-09-16 15:24:44 -0300

    pg

--- src/main/java/com/companyprojectapi/company/services/UserService.java
+++ src/main/java/com/companyprojectapi/company/services/UserService.java
@@ -3,6 +3,8 @@ package com.companyprojectapi.company.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,16 +46,19 @@ public class UserService {
 	}
 	
 	public User uptade(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateDate(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateDate(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-
+	
 	private void updateDate(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 		
 	}
-
 }
